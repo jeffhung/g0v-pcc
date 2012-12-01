cfb80b1a6dd10b695edad3c8c9f05b6fc240454e
Jeff Hung <jeff.cc.hung@gmail.com>
2012-12-01 14:33:12 +0800

    Add PCC parser written by Iamsorry.

---
+++ G0VTW-PCC-Fetch/lib/G0VTW/PCC/Fetch/Parser.pm
@@ -0,0 +1,69 @@
+package G0VTW::PCC::Fetch::Parser;
+
+sub tender_parse
+{
+	my $body = shift;
+
+	my @lines = split(/\n/, $body);
+
+	my $i = 0;
+	my $f_key = 0;
+	while($i <= $#lines)
+	{
+		$line = $lines[$i];
+		if($line =~ /^\s*(\S+<br>\S+<br>\S+<br>\S+)\s*$/)
+		{
+			$section = $1;
+			$section =~ s/<br>//g;
+			print "$section\n";
+		}
+		elsif($line =~ /<th/)
+		{
+			$key = $line;
+			if($line !~ /<\/th/)
+			{ 
+				do
+				{
+					$i += 1;
+					$line = $lines[$i];
+					$key .= $line;
+				}while ($line !~ /<\/th/ && $i <= $#lines);
+			}
+			$key =~ s/[\r\n]//g;
+			$key =~ /<th[^>]*>\s*(.*)\s*<\/th/;
+			$key = $1;
+			$key =~ s/<[^>]+>//g;
+			$key =~ s/\s+/ /g;
+			$f_key = 1;
+			print "\t$key\n";
+		}
+		elsif($line =~ /<td/)
+		{
+			$value = $line;
+			do
+			{
+				$i += 1;
+				$line = $lines[$i];
+				$value .= $line;
+			}while ($line !~ /<\/td/ && $i <= $#lines);
+			$value =~ s/[\r\n]//g;
+			$value =~ /<td[^>]*>\s*(.*)\s*<\/td/;
+			$value = $1;
+			$value =~ s/<[^>]+>//g;
+			$value =~ s/\s+/ /g;
+			if(1 == $f_key)
+			{
+				$f_key = 0;
+				print "\t\t$value\n";
+			}
+		}
+		else
+		{
+			#print "[nonmatch] $line\n";
+		}
+		$i += 1;
+	}
+}
+
+1;
+
