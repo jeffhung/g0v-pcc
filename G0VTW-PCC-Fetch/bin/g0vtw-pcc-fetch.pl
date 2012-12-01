9a32bc2f20f6ccefb23b27072b51b333028a17d1
Jeff Hung <jeff.cc.hung@gmail.com>
2012-12-01 17:07:05 +0800

    Crawl ATM cases.

--- G0VTW-PCC-Fetch/bin/g0vtw-pcc-fetch.pl
+++ G0VTW-PCC-Fetch/bin/g0vtw-pcc-fetch.pl
@@ -2,6 +2,7 @@
 
 use G0VTW::PCC::Fetch::Crawler;
 
-$crawler = new G0VTW::PCC::Fetch::Crawler( data_dir => '2012nov' );
-$crawler->crawl_announcement();
+$crawler = new G0VTW::PCC::Fetch::Crawler( data_dir => 'atm-2012nov' );
+#$crawler->crawl_announcement();
+$crawler->crawl_atm();
 
